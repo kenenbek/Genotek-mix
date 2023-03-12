6f801926f22ce583b9accebccb95c1669c5f03d3
Kenenbek R <kenenbek@gmail.com>
2023-03-13 01:13:48 +0600

    1

--- mix/models.py
+++ mix/models.py
@@ -34,8 +34,8 @@ class AttnGCN(torch.nn.Module):
         h = self.norm(h)
         h = self.conv1(h, edge_index, edge_weight)
         h = h.relu()
-        #h = self.conv2(h, edge_index, edge_weight)
-        #h = h.relu()
+        # h = self.conv2(h, edge_index, edge_weight)
+        # h = h.relu()
         # h = self.conv3(h, edge_index, edge_weight)
         # h = h.relu()
         h = self.fc1(h)
@@ -82,14 +82,9 @@ class GCN(torch.nn.Module):
         self.conv1 = GCNConv(30, 30,
                              add_self_loops=True,
                              normalize=True)
-        self.conv2 = GCNConv(30, 30,
-                             add_self_loops=True,
-                             normalize=True)
         self.fc1 = Linear(30, 30)
         self.fc2 = Linear(30, 30)
-        self.fc3 = Linear(30, 30)
-        self.fc4 = Linear(30, 30)
-        self.fc5 = Linear(30, 10)
+        self.fc3 = Linear(30, 10)
 
     def forward(self, h, edge_index, edge_weight):
         h = self.conv1(h, edge_index, edge_weight)
@@ -101,8 +96,4 @@ class GCN(torch.nn.Module):
         h = self.fc2(h)
         h = h.relu()
         h = self.fc3(h)
-        h = h.relu()
-        h = self.fc4(h)
-        h = h.relu()
-        h = self.fc5(h)
         return h
