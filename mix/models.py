38bd8923f1b23621cb69ca85276389715d21d3c5
Kenenbek R <kenenbek@gmail.com>
2023-03-13 02:45:03 +0600

    1

--- mix/models.py
+++ mix/models.py
@@ -11,7 +11,7 @@ class AttnGCN(torch.nn.Module):
         self.norm = BatchNorm1d(30)
         self.conv1 = GATConv(in_channels=30,
                              out_channels=30,
-                             heads=4,
+                             heads=8,
                              concat=False,
                              add_self_loops=True,
                              edge_dim=1)
