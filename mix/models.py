a4ab7fbbb7b44b297bb5a4c5d962991fb86f3854
Kenenbek R <kenenbek@gmail.com>
2023-03-13 01:16:33 +0600

    1

--- mix/models.py
+++ mix/models.py
@@ -89,8 +89,6 @@ class GCN(torch.nn.Module):
     def forward(self, h, edge_index, edge_weight):
         h = self.conv1(h, edge_index, edge_weight)
         h = h.relu()
-        h = self.conv2(h, edge_index, edge_weight)
-        h = h.relu()
         h = self.fc1(h)
         h = h.relu()
         h = self.fc2(h)
