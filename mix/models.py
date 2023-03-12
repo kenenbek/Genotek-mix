ef8b9f7ea78db5f795ccaae65505de4761832c83
Kenenbek R <kenenbek@gmail.com>
2023-03-12 23:37:25 +0600

    1

--- mix/models.py
+++ mix/models.py
@@ -79,27 +79,17 @@ class GCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
         torch.manual_seed(1234)
-        self.conv1 = GCNConv(30, 30,
+        self.conv1 = GCNConv(30, 60,
                              add_self_loops=True,
                              normalize=True)
-        self.conv2 = GCNConv(30, 30,
-                             add_self_loops=True,
-                             normalize=True)
-        self.conv3 = GCNConv(30, 30,
-                             add_self_loops=True,
-                             normalize=True)
-        self.fc1 = Linear(30, 30)
-        self.fc2 = Linear(30, 30)
-        self.fc3 = Linear(30, 10)
-        self.fc4 = Linear(10, 10)
-        self.fc5 = Linear(10, 10)
+        self.fc1 = Linear(60, 60)
+        self.fc2 = Linear(60, 60)
+        self.fc3 = Linear(60, 60)
+        self.fc4 = Linear(60, 60)
+        self.fc5 = Linear(60, 10)
 
     def forward(self, h, edge_index, edge_weight):
         h = self.conv1(h, edge_index, edge_weight)
-        h = h.relu()
-        h = self.conv2(h, edge_index, edge_weight)
-        h = h.relu()
-        h = self.conv3(h, edge_index, edge_weight)
         h = h.relu()
         h = self.fc1(h)
         h = h.relu()
