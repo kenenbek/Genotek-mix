06d54b1a226ac9e60c2b5f536410fe4e4a092754
Kenenbek R <kenenbek@gmail.com>
2023-03-13 01:23:19 +0600

    1

--- mix/models.py
+++ mix/models.py
@@ -11,42 +11,22 @@ class AttnGCN(torch.nn.Module):
         self.norm = BatchNorm1d(30)
         self.conv1 = GATConv(in_channels=30,
                              out_channels=30,
-                             heads=2,
+                             heads=1,
                              add_self_loops=True,
                              edge_dim=1)
-        # self.conv2 = GATConv(in_channels=60,
-        #                      out_channels=30,
-        #                      heads=2,
-        #                      add_self_loops=True,
-        #                      edge_dim=1)
-        # self.conv3 = GATConv(in_channels=66,
-        #                      out_channels=66,
-        #                      heads=1,
-        #                      add_self_loops=False,
-        #                      edge_dim=1)
-        self.fc1 = Linear(60, 30)
+        self.fc1 = Linear(30, 30)
         self.fc2 = Linear(30, 30)
         self.fc3 = Linear(30, 10)
-        self.fc4 = Linear(10, 10)
-        self.fc5 = Linear(10, 10)
 
     def forward(self, h, edge_index, edge_weight):
         h = self.norm(h)
         h = self.conv1(h, edge_index, edge_weight)
         h = h.relu()
-        # h = self.conv2(h, edge_index, edge_weight)
-        # h = h.relu()
-        # h = self.conv3(h, edge_index, edge_weight)
-        # h = h.relu()
         h = self.fc1(h)
         h = h.relu()
         h = self.fc2(h)
         h = h.relu()
         h = self.fc3(h)
-        h = h.relu()
-        h = self.fc4(h)
-        h = h.relu()
-        h = self.fc5(h)
         return h
 
 
