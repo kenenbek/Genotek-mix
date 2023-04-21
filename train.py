a1a5f051dae67b5707ad1aee8fcca3af4dad5d38
Kenenbek R <kenenbek@gmail.com>
2023-04-22 03:19:22 +0600

    1

--- train.py
+++ train.py
@@ -40,10 +40,10 @@ def custom_loss(embeddings, edge_index, edge_weights, labels):
     same_class_mask = (labels[src] == labels[dest])
     diff_class_mask = ~same_class_mask
 
-    same_class_loss = - edge_weights[same_class_mask] * embeddings_dot[same_class_mask]
-    diff_class_loss = edge_weights[diff_class_mask] * embeddings_dot[diff_class_mask]
+    same_class_loss = - torch.mean(edge_weights[same_class_mask] * embeddings_dot[same_class_mask])
+    diff_class_loss = torch.mean(edge_weights[diff_class_mask] * embeddings_dot[diff_class_mask])
 
-    loss = torch.mean(same_class_loss) + torch.mean(diff_class_loss)
+    loss = same_class_loss + diff_class_loss
 
     return loss
 
